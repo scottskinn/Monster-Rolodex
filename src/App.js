a80e1b8a97fa6fb571df91255a06f2259be79abb
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-05-25 10:21:35 -0400

    serach input component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { Component } from 'react';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
       monsters: [],
       searchField: ''
     };
-    console.log("constructor")
+    // console.log("constructor")
   }
 
   componentDidMount() {
-    console.log("componentDidMount")
+    // console.log("componentDidMount")
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((user) => 
@@ -34,32 +36,23 @@ class App extends Component {
   }
 
   render() {
-    console.log("render")
-
+    // console.log("render")
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
-    
+
     const filterMonster = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
-        <input 
-          className='search-box' 
-          type='search' 
-          placeholder='search monsters' 
-          onChange={onSearchChange}
+        <SearchBox 
+          className='search-box'
+          onChangeHandler={onSearchChange}
+          placeholder='search monsters'
         />
-        {
-          filterMonster.map((monster) => {
-            return (
-              <h1 key={monster.id}>
-              {monster.name}
-              </h1>
-            )
-          })
-        }
+
+        <CardList monsters={filterMonster} />
       </div>
     );
   }
